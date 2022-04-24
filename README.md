3959ef253bdef5286524f23d05c0c34581bbf4e3
krleitch <krleitch.ca@gmail.com>
2022-04-24 00:22:39 -0400

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# nvim-lychee
+
+-   A copy of https://github.com/catppuccin/nvim with my own colour scheme
+-   In progress of making it more unique
---
+++ lua/catppuccin/core/color_palette.lua
@@ -0,0 +1,26 @@
+local color_palette = {
+	rosewater = "#F5E0DC", -- Rosewater
+	flamingo = "#F2CDCD", -- Flamingo
+	mauve = "#ce9178", -- Mauve RETURN
+	pink = "#d9af94", -- Pink IMPORTS
+	red = "#F28FAD", -- Red CONDITIONAL
+	maroon = "#E8A2AF", -- Maroon
+	peach = "#FFCDD2", -- Peach PROPS
+	yellow = "#b2dfdb", -- Yellow THIS.
+	green = "#c5e1a5", -- Green COMMENTS
+	blue = "#e57373", -- Blue FUNCTIONS
+	sky = "#89DCEB", -- Sky
+	teal = "#B5E8E0", -- Teal
+	lavender = "#C9CBFF", -- Lavender
+	white = "#dad0ce", -- White TEXT
+	gray2 = "#C3BAC6", -- Gray2
+	gray1 = "#988BA2", -- Gray1
+	gray0 = "#637985", -- Gray0
+	black4 = "#575268", -- Black4
+	black3 = "#302D41", -- Black3
+	black2 = "#0b0c0f", -- Black2
+	black1 = "#1A1826", -- Black1
+	black0 = "#161320", -- Black0
+}
+
+return color_palette
