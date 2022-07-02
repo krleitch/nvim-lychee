e3ee26c3a3965a33589d1516a3e856990a144544
krleitch <krleitch.ca@gmail.com>
2022-07-02 01:01:31 -0400

    add alpha startup colors integration

---
+++ lua/lychee/core/integrations/alpha.lua
@@ -0,0 +1,17 @@
+local M = {}
+
+function M.get(cp)
+	return {
+      -- Dashboard header colors
+    StartLogo1 = { fg = "#1C506B" },
+    StartLogo2 = { fg = "#1D5D68" },
+    StartLogo3 = { fg = "#1E6965" },
+    StartLogo4 = { fg = "#1F7562" },
+    StartLogo5 = { fg = "#21825F" },
+    StartLogo6 = { fg = "#228E5C" },
+    StartLogo7 = { fg = "#239B59" },
+    StartLogo8 = { fg = "#24A755" },
+	}
+end
+
+return M
