64aa9201ff75d4b0c58913394da0c5938e7f54e2
krleitch <krleitch.ca@gmail.com>
2022-07-02 01:12:23 -0400

    alpha startup colors

--- lua/lychee/core/integrations/alpha.lua
+++ lua/lychee/core/integrations/alpha.lua
@@ -3,14 +3,14 @@ local M = {}
 function M.get(cp)
 	return {
       -- Dashboard header colors
-    StartLogo1 = { fg = "#1C506B" },
-    StartLogo2 = { fg = "#1D5D68" },
-    StartLogo3 = { fg = "#1E6965" },
-    StartLogo4 = { fg = "#1F7562" },
-    StartLogo5 = { fg = "#21825F" },
-    StartLogo6 = { fg = "#228E5C" },
-    StartLogo7 = { fg = "#239B59" },
-    StartLogo8 = { fg = "#24A755" },
+    StartLogo1 = { fg = "#e34d8f" },
+    StartLogo2 = { fg = "#df337f" },
+    StartLogo3 = { fg = "#db1a6f" },
+    StartLogo4 = { fg = "#d7005f" },
+    StartLogo5 = { fg = "#c20056" },
+    StartLogo6 = { fg = "#ac004c" },
+    StartLogo7 = { fg = "#970043" },
+    StartLogo8 = { fg = "#810039" },
 	}
 end
 
